62228ca78f9d09a77745533b3330545fd976854a
dalramjwi <dalramjwi@gmail.com>
2024-06-26 17:13:32 +0900

    console

--- arrayTest.ts
+++ arrayTest.ts
@@ -33,6 +33,7 @@ const item2List = items2.map((item: Item2) => {
 });
 console.log(item2List);
 //재고가 많은 순서대로 상품 정렬
+// JavaScript의 배열 객체에서 제공하는 sort() 메서드는 배열의 요소를 정렬하는 데 사용됩니다. 이 메서드는 기본적으로 배열의 요소를 문자열로 변환한 후 유니코드 코드 포인트 순서로 정렬합니다. 그러나 숫자를 정렬할 때는 이 점에 유의해야 하며, 비교 함수를 제공하여 정렬 순서를 사용자가 정의할 수 있습니다.
 const items3: Item[] = [
   { name: "콜라", stock: 5 },
   { name: "사이다", stock: 0 },
