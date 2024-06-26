f4014c34014c11670ed26223e7304a89e6233acd
dalramjwi <dalramjwi@gmail.com>
2024-06-26 10:36:23 +0900

    배열 자체 비교 시도

--- test.ts
+++ test.ts
@@ -2,6 +2,9 @@ const a = 1;
 const b = 1;
 console.log(a === b);
 const c = [1];
-//변수 C의 index 0에
+//변수 C의 index 0에 값
 const d = [1];
 console.log(c[0] === d[0]);
+//index 값이 같아?
+console.log(c === d);
+//배열이 같아?
